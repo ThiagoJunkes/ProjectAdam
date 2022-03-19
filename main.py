42e482ccd7c2668deb49b4137e936b9966a4c3dc
ThiagoJunkes <thiago.junkes3@gmail.com>
2022-03-19 13:27:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 """Project Adam    By: Thiago Junkes"""
 
+from multiprocessing.connection import Listener
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+
+try:
+    with sr.Microphone() as source:
+        print("Ouvindo...")
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(command)
+except:
+    pass
+
